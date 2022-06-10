cc17fb2efcfd859cf7bf8781e614d9262d1e17d9
ElviraZharenkova <elvira_bitl@list.ru>
2022-06-10 18:41:50 +0800

    DZ lesson7 Reflection

--- src/main/java/lesson7/MainApp7.java
+++ src/main/java/lesson7/MainApp7.java
@@ -1,4 +1,9 @@
 package lesson7;
 
 public class MainApp7 {
+    public static void main(String[] args) {
+
+        TestClass testClass = new TestClass();
+        HandlerTest.start(testClass.getClass());
+    }
 }
